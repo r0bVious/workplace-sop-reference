71980e0752e524bd22b71212356463069c0f3a3b
r0bVious <robert.spano@gmail.com>
2024-09-03 18:52:31 +0900

    keeping server awake

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -5,6 +5,9 @@ import commentsRoutes from "./comments-routes.js";
 
 const appRouter = Router();
 
+appRouter.use("/wake", (req, res) => {
+  res.status(200).send("Server is awake!");
+});
 appRouter.use("/user", userRoutes);
 appRouter.use("/articles", articleRoutes);
 appRouter.use("/comments", commentsRoutes);
